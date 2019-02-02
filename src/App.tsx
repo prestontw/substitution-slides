220dc88fbde724f121ca1c4e363188dbc8975bf2
Preston TW <preston_tunnell_wilson@brown.edu>
2019-02-02 16:50:15 -0500

    integrate current reference with replace text
    need to figure out how to get last position of codemirror

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,12 @@ interface ComponentProgram {
   post: string,
 }
 
+interface Selection {
+  from: CodeMirror.Position;
+  to: CodeMirror.Position;
+}
 interface State {
   steps: ComponentProgram[];
-  selection?: { from: CodeMirror.Position, to: CodeMirror.Position };
   reference?: IInstance;
   replacement?: IInstance;
 }
@@ -29,13 +32,32 @@ class App extends React.Component<Props, State> {
   }
 
   replaceText() {
-    let result = (this.state.replacement != undefined)? this.state.replacement.getValue() : "WHOOPS, could not read your result";
     // get selection from reference, get pre and post,
     // add new thing to steps,
-    this.state.steps.push({pre: "", highlight: "", result, post: ""});
-    this.forceUpdate();
-    console.log(this.state.steps);
-    console.log("hello");
+    let selection = this.getSelection(this.state.reference);
+    if (selection != undefined) {
+      let pre = this.state.reference!.getRange({line: 0, ch: 0}, selection.from);
+      let highlight = this.state.reference!.getRange(selection.from, selection.to);
+      let result = (this.state.replacement != undefined) ? this.state.replacement.getValue() : "WHOOPS, could not read your result";
+      let end = {ch: 0, line: 0};
+      let post = this.state.reference!.getRange(selection.to, end);
+
+      this.state.steps.push({ pre, highlight, result, post });
+      this.forceUpdate();
+      this.state.replacement!.setValue("");
+    }
+    else {
+      return;
+    }
+  }
+
+  getSelection(editor?: IInstance): Selection | undefined {
+    if (editor != undefined) {
+      return { from: editor.getCursor("start"), to: editor.getCursor("end") };
+    }
+    else {
+      return undefined;
+    }
   }
 
   programToString(p: ComponentProgram): string {
@@ -44,15 +66,15 @@ class App extends React.Component<Props, State> {
   previousCode(): string {
     let len = this.state.steps.length;
     console.log(len);
-    return (len > 0)? this.programToString(this.state.steps[len - 1]) : "";
+    return (len > 0) ? this.programToString(this.state.steps[len - 1]) : "";
   }
 
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <button onClick={() => {console.log("hello")}}>Select Text</button>
-          <button onClick={() => {this.replaceText()}}>Replace text</button>
+          <button onClick={() => { console.log("hello") }}>Select Text</button>
+          <button onClick={() => { this.replaceText() }}>Replace text</button>
         </header>
         <PreviousStep code={this.previousCode()} onMount={editor => { this.setState({ ... this.state, reference: editor }) }} />
         <ResultingCode onMount={editor => {
